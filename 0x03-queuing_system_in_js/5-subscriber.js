bf4bdd28a81ecffed63c8c2ff491f53e87391361
JosiahMbithi001 <mbithishucks@gmail.com>
2023-09-01 11:16:01 -0700

    Task 4-9

--- 0x03-queuing_system_in_js/5-subscriber.js
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -1,7 +1,9 @@
+
 #!/usr/bin/yarn dev
 import { createClient } from 'redis';
 
 const client = createClient();
+const EXIT_MSG = 'KILL_SERVER';
 
 client.on('error', (err) => {
   console.log('Redis client not connected to the server:', err.toString());
@@ -10,3 +12,14 @@ client.on('error', (err) => {
 client.on('connect', () => {
   console.log('Redis client connected to the server');
 });
+
+client.subscribe('holberton school channel');
+
+client.on('message', (_err, msg) => {
+  console.log(msg);
+  if (msg === EXIT_MSG) {
+    client.unsubscribe();
+    client.quit();
+  }
+});
+
